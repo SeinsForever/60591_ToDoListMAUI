746c1fde676e1931ab8eada92bb086806d4b5bfd
poddu <ivan_poddubnyy01@mail.ru>
2022-12-13 01:30:19 +0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+        BindingContext = new MainViewModel();
+    }
+}
+
---
+++ MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ToDoList;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        Console.Write("Hello");
+    }
+
+    [ObservableProperty]
+    string newTaskText;
+
+    [ObservableProperty] 
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    ObservableCollection<ToDoItem> taskList = new ObservableCollection<ToDoItem>();
+
+    public bool IsNotBusy => !isBusy;
+
+
+    [RelayCommand]
+    void AddNewTask()
+    {
+        IsBusy = true;
+        
+        TaskList.Add(new ToDoItem(){TaskName = NewTaskText});
+        NewTaskText = "";
+
+        IsBusy = false;
+    }
+}
+
+public partial class ToDoItem : ObservableObject
+{
+    [ObservableProperty]
+    string taskName;
+
+    [ObservableProperty]
+    bool done = false;
+}
+
+
+
+
